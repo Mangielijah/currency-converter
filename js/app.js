b7a51806ee13c59907b37dfb9b17b253f7bf40c7
Mangi elijah <mangielijah8@gmail.com>
2018-06-30 22:09:40 +0100

    app install

--- js/app.js
+++ js/app.js
@@ -11,6 +11,47 @@ const toDisp = document.querySelector('#to');
 const Disp = document.querySelector('#display');
 const rate = document.querySelector('#unit-rate');
 const curDisp = document.querySelector('#currency');
+const btnAdd = document.querySelector('#btnAdd');
+
+//adding mobile install popup button to app
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'none';
+
+});
+
+
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
+
+//if app installation was succesfull
+window.addEventListener('appinstalled', (evt) => {
+  app.logEvent('a2hs', 'installed');
+});
+
+
 
 window.addEventListener('load', (e) => {
 
